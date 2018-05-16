0408a4d9297f4ee5bcd1b1036bf9157fb09629ad
joseph7802 <jlee7802@gmail.com>
2018-05-16 00:05:15 -0400

    Added user permission for location.

--- app/src/main/java/com/litty/litty2/MainActivity.java
+++ app/src/main/java/com/litty/litty2/MainActivity.java
@@ -1,12 +1,22 @@
 package com.litty.litty2;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.Manifest;
+
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import android.content.pm.PackageManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener{
     private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
@@ -16,16 +26,40 @@ public class MainActivity extends AppCompatActivity {
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        if (mRequestingLocationUpdates)
-            startLocationUpdates();
+        startLocationUpdates();
+    }
+
+    public boolean checkLocationPermission() {
+        //String permission = "android.permission.ACCESS_FINE_LOCATION";
+        String permission = Manifest.permission.ACCESS_FINE_LOCATION;
+        int res = this.checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
     }
 
     protected void startLocationUpdates() {
+        LocationCallback mLocationCallback = new LocationCallback();
         LocationRequest mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(10000);
         mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback,null /* Looper */);
+        if (checkLocationPermission())
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback,null);
+    }
+
+    public void onConnected(Bundle conn) {
+
+    }
+
+    public void onConnectionFailed(ConnectionResult result) {
+
+    }
+
+    public void onLocationChanged(Location location) {
+
+    }
+
+    public void onConnectionSuspended(int cause) {
+
     }
 }
