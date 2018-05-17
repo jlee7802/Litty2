2fa6845a89de1397424bda7cb040c966ea7c06dd
joseph7802 <jlee7802@gmail.com>
2018-05-17 01:20:30 -0400

    Added user permission for location.

--- app/src/main/java/com/litty/litty2/MainActivity.java
+++ app/src/main/java/com/litty/litty2/MainActivity.java
@@ -1,12 +1,20 @@
 package com.litty.litty2;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.location.Location;
+import android.app.Activity;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.Manifest;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationListener;
@@ -18,6 +26,8 @@ import android.content.pm.PackageManager;
 public class MainActivity extends AppCompatActivity implements
         ConnectionCallbacks, OnConnectionFailedListener, LocationListener{
     private FusedLocationProviderClient mFusedLocationClient;
+    protected LocationCallback mLocationCallback = new LocationCallback();
+    protected Location mCurrentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,25 +36,44 @@ public class MainActivity extends AppCompatActivity implements
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        /*ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);*/
+
         startLocationUpdates();
     }
 
-    public boolean checkLocationPermission() {
-        //String permission = "android.permission.ACCESS_FINE_LOCATION";
-        String permission = Manifest.permission.ACCESS_FINE_LOCATION;
-        int res = this.checkCallingOrSelfPermission(permission);
-        return (res == PackageManager.PERMISSION_GRANTED);
-    }
-
     protected void startLocationUpdates() {
-        LocationCallback mLocationCallback = new LocationCallback();
         LocationRequest mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(10000);
         mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        if (checkLocationPermission())
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback,null);
+
+            mLocationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult == null) {
+                        TextView textView = findViewById(R.id.textView);
+                        textView.setText(String.valueOf(999));
+                        return;
+                    }
+                    for (Location location : locationResult.getLocations()) {
+                        TextView textView = findViewById(R.id.textView);
+                        textView.setText(String.valueOf(99999));
+
+                        TextView textView2 = findViewById(R.id.textView3);
+                        textView2.setText(String.valueOf(location.getLatitude()));
+                    }
+                };
+            };
+        }
+        else {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+          /*  Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);*/ //Might not need this
+        }
     }
 
     public void onConnected(Bundle conn) {
@@ -56,7 +85,7 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     public void onLocationChanged(Location location) {
-
+        mCurrentLocation = location;
     }
 
     public void onConnectionSuspended(int cause) {
